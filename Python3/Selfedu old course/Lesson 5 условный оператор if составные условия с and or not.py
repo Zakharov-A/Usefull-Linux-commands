de130b5d3b5c7de1389204e2263ab6b58fb191f2
Andrei Zakharov <redzed427@gmail.com>
2021-09-11 23:55:05 +0300

    Python3-course-11-09-21

--- Python3/Selfedu old course/Lesson 5 условный оператор if составные условия с and or not.py
+++ Python3/Selfedu old course/Lesson 5 условный оператор if составные условия с and or not.py
@@ -27,10 +27,36 @@
     
 #Добавлены проверка
 
-x = int(input('Введите число: '))
-if x < 0 :
-    print("x отрицательное число")
-elif x > 0:
-    print("x положительное число")
-else:
-    print("x равен 0")
+#x = int(input('Введите число: '))
+#if x < 0 :
+    #print("x отрицательное число")
+#elif x > 0:
+    #print("x положительное число")
+#else:
+    #print("x равен 0")
+
+#x = int(input("Введите число: "))
+#sgn = 0
+#if x < 0 :
+    #sgn = -1
+    #print("x отрицательное число", sgn)
+#elif x > 0:
+    #sgn = 1
+    #print("x положительное число", sgn)
+#else:
+    #print("x равен 0", sgn)
+
+#age = 8
+#accessAllowed = True if age >= 18 else False
+#print(accessAllowed)
+
+#x = 40
+#if x >= 2 and x <=7 : print("x попадает в [2; 7]")
+#else: print("x не попадает в [2; 7]")
+
+
+x = 4
+if(x < 2 or x > 7): print("x попадает в [2; 7]")
+else: print("x не попадает в [2; 7]")
+
+    
