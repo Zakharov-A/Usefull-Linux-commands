f804688a5a5e943c16571532af8d005512efc0d2
Andrei Zakharov <redzed427@gmail.com>
2021-09-12 11:04:44 +0300

    Python-course-12-21

--- Python3/Selfedu old course/Lesson 5 условный оператор if составные условия с and or not.py
+++ Python3/Selfedu old course/Lesson 5 условный оператор if составные условия с and or not.py
@@ -50,13 +50,22 @@
 #accessAllowed = True if age >= 18 else False
 #print(accessAllowed)
 
-#x = 40
+#x = 4
 #if x >= 2 and x <=7 : print("x попадает в [2; 7]")
 #else: print("x не попадает в [2; 7]")
 
 
-x = 4
-if(x < 2 or x > 7): print("x попадает в [2; 7]")
-else: print("x не попадает в [2; 7]")
+#x = 4; y = -2
+#if x >= 2 and x <= 7 and (y < 0 or y > 5):
+    #print("x попадает в [2; 7], y не попадает в [0; 5]")
+    
+
+x = 4; y = True; z = False
+if(x): print("x = ", x, " дает true")
+if(not 0): print("0 дает false")
+if("0"): print("Строка 0 дает true")
+if(not "") :print("пустая строка дает false")
+if(y): print("y = true дает true")
+if(not z): print("z = false дает false")
 
     
