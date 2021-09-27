c76ced31fc5351336d5bc528bf7b45c3ec21f95e
Andrew_Zakharov <redzed427@gmail.com>
2021-09-27 08:47:34 +0300

    Py-old-curse-l6

---
+++ Python3/Selfedu old course/Lesson 6 операторы циклов while and for.py
@@ -0,0 +1,80 @@
+#Первый пример
+
+#s=0; i=1
+#while i <= 1000 and s < 5:
+    #s +=1/i
+    #i += 1
+#print(s)
+
+#Второй пример пропущен
+
+
+#Третий пример
+
+#s=0; i=10
+#while i < 100 :
+    #if i == 0: break
+    #s += 1/i
+    #i=i+1
+#else:
+    #print("Сумма вычеслена корректно")
+#print(s)
+
+
+#Четвертый пример
+
+#s=0; i=-5
+#while i < 4 :
+    #i=i+1
+    #if i == 0: continue
+    #print(i)
+    #s += 1/i
+#print(s)
+
+#Пятый пример
+
+#for x in 1,2,3,4:
+    #print(x**2)
+
+#Шестой пример
+
+#for x in range(0, 5, 2):
+    #print(x)
+
+#Седьмой пример
+
+#s=0
+#for i in range(1, 1001,1): s += 1/i
+#print(s)
+
+#Восьмой пример
+
+#k = 0.5; b = 2
+#lst = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+#for x in lst:
+    #print(x*k+b)
+    
+#Девятый пример
+
+#msg = "Hello world"
+#for x in msg :
+    #print(x)
+
+#Десятый пример
+
+#A = [ [1,2,3], [4,5,6] ]
+#N=2; M=3
+#for i in range(N):
+    #for j in range(M):
+        #print(A[i] [j], end='')
+    #print()
+#Одинацатый пример
+
+S=0; M=10; N=5
+for i in range(1,N+1):
+    for j in range(1,M+1):
+        S += i*j
+print(S)
+
+    
+
