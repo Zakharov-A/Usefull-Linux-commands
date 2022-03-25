13f6f4bc640733b18f13fabacb580fcb06bd73a2
Dredd <redzed427@gmail.com>
2022-03-25 07:59:39 -0400

    Lin_Kali_mac_changer_02_11

--- Linux-Kali/mac_changer/mac_chan.py
+++ Linux-Kali/mac_changer/mac_chan.py
@@ -2,6 +2,11 @@
 
 import subprocess
 
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:66", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+interface = "eth0"
+new_mac = "00:11:22:33:44:77"
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+# subprocess.call("ifconfig eth0 down", shell=True)
+# subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
+# subprocess.call("ifconfig eth0 up", shell=True)
