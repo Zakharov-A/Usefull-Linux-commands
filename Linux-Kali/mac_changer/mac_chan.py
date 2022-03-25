dedca97a461dd6a8b3348e1690d9d2b52b978257
Darth_Maul <redzed427@gmail.com>
2022-03-25 08:56:15 +0300

    Ubuntu_Sec_Test_L_11

--- Linux-Kali/mac_changer/mac_chan.py
+++ Linux-Kali/mac_changer/mac_chan.py
@@ -2,11 +2,11 @@
 
 import subprocess
 
-interface = "eth0"
+interface = "enp0s3"
 new_mac = "00:11:22:33:44:77"
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-# subprocess.call("ifconfig eth0 down", shell=True)
-# subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
-# subprocess.call("ifconfig eth0 up", shell=True)
+# subprocess.call("ifconfig enp0s3 down", shell=True)
+# subprocess.call("ifconfig enp0s3 hw ether 00:11:22:33:44:55", shell=True)
+# subprocess.call("ifconfig enp0s3 up", shell=True)
