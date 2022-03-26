18127ff9b8ddab45eb187a8df9c8aaead9f64ed6
Darth_Maul <redzed427@gmail.com>
2022-03-26 21:49:31 +0300

    Ubuntu_Sec_Test_L_14_input

--- Linux-Kali/mac_changer/Ubuntu/mac_chan.py
+++ Linux-Kali/mac_changer/Ubuntu/mac_chan.py
@@ -1,14 +1,20 @@
-#!/usr/bin/env/ python
+#!/usr/bin/env python
 
 import subprocess
 
 # interface = "enp0s3"
 interface = input("interface > ")
 # new_mac = "08:00:27:FA:04:DF"
+# new_mac = "08:00:27:FA:04:33"
 new_mac = input("New MAC > ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call(" ifconfig " + interface + " down ", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call(" ifconfig " + interface + " up ", shell=True)
+# subprocess.call(" ifconfig " + interface + " down ", shell=True)
+# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+# subprocess.call(" ifconfig " + interface + " up ", shell=True)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
+
