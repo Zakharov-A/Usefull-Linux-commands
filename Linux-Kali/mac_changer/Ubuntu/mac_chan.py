844ab1024eed02404983a29342294baca4eb62d8
Darth_Maul <redzed427@gmail.com>
2022-03-27 04:04:36 +0300

    Ubuntu_Sec_Test_L_12

--- Linux-Kali/mac_changer/Ubuntu/mac_chan.py
+++ Linux-Kali/mac_changer/Ubuntu/mac_chan.py
@@ -7,6 +7,6 @@ new_mac = "00:11:22:33:44:55"
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-# subprocess.call("ifconfig enp0s3 down", shell=True)
-# subprocess.call("ifconfig enp0s3 hw ether 00:11:22:33:44:55", shell=True)
-# subprocess.call("ifconfig enp0s3 up", shell=True)
+subprocess.call(" ifconfig " + interface + " down ", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call(" ifconfig " + interface + " up ", shell=True)
