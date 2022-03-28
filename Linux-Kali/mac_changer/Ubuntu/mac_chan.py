72cc4c07079c2100d97004c4052e476871bc585f
Darth_Maul <redzed427@gmail.com>
2022-03-28 07:29:19 +0300

    Seccurity_Test_L_15

--- Linux-Kali/mac_changer/Ubuntu/mac_chan.py
+++ Linux-Kali/mac_changer/Ubuntu/mac_chan.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+
+parser.parse_args()
 
 # interface = "enp0s3"
 interface = input("interface > ")
@@ -10,9 +17,6 @@ new_mac = input("New MAC > ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-# subprocess.call(" ifconfig " + interface + " down ", shell=True)
-# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-# subprocess.call(" ifconfig " + interface + " up ", shell=True)
 
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
