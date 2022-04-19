722e7799b5102211d5deca7c9e668c42d1344332
Darth <redzed427@gmail.com>
2022-04-19 18:57:29 -0400

    Py_Sec_L_19_and_20

---
+++ mac_changer/Ubuntu/mac_chan.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info")
+    return options
+
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
+
+# interface = "enp0s3"
+# interface = options.interface
+# new_mac = 12:34:56:78:91:02
+# new_mac = 08:00:27:9f:4f:ed
+# new_mac = options.new_mac
+
+
+
